b85ff8e041203f3c5a39297561ac887d9f1bca4d
GerhardArce <dylanarce6@outlook.com>
2023-11-17 17:46:10 -0500

    act 2

---
+++ punto-2/index.php
@@ -0,0 +1,13 @@
+<?php
+    //ENTRY DATA
+    $number = 9;
+    const Numbertwo = 5;
+    
+    echo "La suma es= " . ($number + Numbertwo) . "\n";
+
+    $number = 4;
+    echo "La suma es= " . ($number + Numbertwo) . "\n";
+
+    $number = 6546;
+    echo "La suma es= " . ($number + Numbertwo) . "\n";    
+?>
